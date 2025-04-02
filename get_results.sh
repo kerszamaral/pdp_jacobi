b04aaebfa40e2340494016999b2631a9d7656e50
Ian Kersz <kerszamaral@gmail.com>
2025-04-02 15:49:19 -0300

    Fixed vtune

--- get_results.sh
+++ get_results.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # Lista de pares de entrada
-declare -a input_pairs=("512 1000" "1024 1000" "1024 2000" "1024 10000" "2028 15000")
+declare -a input_pairs=(
+    "512 1000"
+    "1024 1000"
+    "1024 2000"
+    "1024 10000"
+    "2028 15000"
+)
 
 # Lista de números de threads para OpenMP (modifique conforme necessário)
 real_threads=$(nproc --all)
-max_factor="1.5"
+max_factor="2"
 max_threads=$(awk "function ceil(x){return int(x)+(x>int(x))} BEGIN {print ceil($real_threads*$max_factor)}")
 echo "Max threads: $max_threads - Real threads: $real_threads"
 # Verifica se o número máximo de threads é maior que 0
@@ -42,7 +48,8 @@ if [ -p $prog_stdin ]; then
     rm $prog_stdin
 fi
 
-VTUNE_ACTIVE=1
+VTUNE_ACTIVE=0
+vtune_type="hotspots"
 vtune_out="./vtune_out/"
 # Intel VTune Profiler CLI
 # performance-snapshot, hotspots and/or hpc-performance
@@ -58,54 +65,23 @@ fi
 # Executa os testes sequenciais e salva os resultados
 for pair in "${input_pairs[@]}"; do
     echo "Running laplace_seq with input: $pair"
-    # Creates the stdin pipe
-    mkfifo $prog_stdin
     out_file="$out_dir/laplace_seq_${pair// /_}.txt"
-    # Executa o programa em segundo plano
-    ./laplace_seq < $prog_stdin > $out_file 2>&1 &
-    # Captura o PID do processo em segundo plano
-    ppid=$!
-
     # Executa o VTune em segundo plan
+    start_time=$(date +%s.%N)
     if [ $VTUNE_ACTIVE -eq 1 ]; then
         # Executa o VTune em segundo plano
         vtune_output="$vtune_out/vtune_${pair// /_}_seq"
-        vtune_output_hpc="${vtune_output}_hpc"
-        vtune_output_hs="${vtune_output}_hs"
-        vtune_output_ps="${vtune_output}_ps"
-        # Launches the analysis tool with the process ID
-        vtune -collect hotspots -r $vtune_output_hs -target-pid $ppid &> hs.out &
-        hspid=$!
-        # vtune -collect hpc-performance -r $vtune_output_hpc -target-pid $ppid &> hpc.out &
-        # hpcpid=$!
-        # vtune -collect performance-snapshot -r $vtune_output_ps -target-pid $ppid &> ps.out &
-        # pspid=$!
-        
-        while [[ -z $(grep "Collection started" hs.out) ]]; do
-            # || [[ -z $(grep "Collection started" hpc.out) ]]
-            # || [[ -z $(grep "Collection started" ps.out) ]]
-            sleep 0.1  # Os processos do VTune podem demorar para iniciar
-        done
+        # Launches the analysis tool with the process
+        vtune -collect $vtune_type -r $vtune_output ./laplace_seq -- $pair &> $out_file
+    else
+        # Executa o programa em segundo plano
+        ./laplace_seq $pair &> $out_file
     fi
-
-    # Aguarda o processo em segundo plano ser iniciado
-    start_time=$(date +%s.%N)
-    echo "$pair" > $prog_stdin
-    wait $ppid
     end_time=$(date +%s.%N)
 
-    # Aguarda o VTune terminar
-    if [ $VTUNE_ACTIVE -eq 1 ]; then
-        wait $hspid
-        # wait $hpcpid
-        # wait $pspid
-    fi
-
-    # Remove o pipe
-    rm $prog_stdin
     # Calcula o tempo de execução
     exec_time_seq=$(echo "$end_time - $start_time" | bc)
-    echo "$ppid: $pair $exec_time_seq"
+    echo "$pair $exec_time_seq"
     echo "$pair $exec_time_seq" >> "$output_file"
 done
 
@@ -116,54 +92,24 @@ for num_threads in "${num_threads_list[@]}"; do
     for pair in "${input_pairs[@]}"; do
         echo "Running laplace_mp with input: $pair and $num_threads threads"
         # Cria o pipe para a entrada padrão
-        mkfifo $prog_stdin
         out_file="$out_dir/laplace_mp_${pair// /_}_$num_threads.txt"
-
-        # Executa o programa em segundo plano
-        ./laplace_mp < $prog_stdin > $out_file 2>&1 &
-        # Captura o PID do processo em segundo plano
-        ppid=$!
-
-        # Executa o VTune em segundo plano
+        
+        # Executa o VTune em segundo plan
+        start_time=$(date +%s.%N)
         if [ $VTUNE_ACTIVE -eq 1 ]; then
             # Executa o VTune em segundo plano
             vtune_output="$vtune_out/vtune_${pair// /_}_mp_${num_threads}"
-            vtune_output_hpc="${vtune_output}_hpc"
-            vtune_output_hs="${vtune_output}_hs"
-            vtune_output_ps="${vtune_output}_ps"
-            # Launches the analysis tool with the process ID
-            vtune -collect hotspots -r $vtune_output_hs -target-pid $ppid &> hs.out &
-            hspid=$!
-            # vtune -collect hpc-performance -r $vtune_output_hpc -target-pid $ppid &> hpc.out &
-            # hpcpid=$!
-            # vtune -collect performance-snapshot -r $vtune_output_ps -target-pid $ppid &> ps.out &
-            # pspid=$!
-
-            while [[ -z $(grep "Collection started" hs.out) ]]; do
-                # || [[ -z $(grep "Collection started" hpc.out) ]]
-                # || [[ -z $(grep "Collection started" ps.out) ]]
-                sleep 0.1  # Os processos do VTune podem demorar para iniciar
-            done
+            # Launches the analysis tool with the process
+            vtune -collect $vtune_type -r $vtune_output ./laplace_mp -- $pair &> $out_file
+        else
+            # Executa o programa em segundo plano
+            ./laplace_mp $pair &> $out_file
         fi
-
-        # Aguarda o processo em segundo plano ser iniciado
-        start_time=$(date +%s.%N)
-        echo "$pair" > $prog_stdin
-        wait $ppid
         end_time=$(date +%s.%N)
 
-        # Aguarda o VTune terminar
-        if [ $VTUNE_ACTIVE -eq 1 ]; then
-            wait $hspid
-            # wait $hpcpid
-            # wait $pspid
-        fi
-
-        # Remove o pipe
-        rm $prog_stdin
         # Calcula o tempo de execução
         exec_time_mp=$(echo "$end_time - $start_time" | bc)
-        echo "$ppid: $pair $exec_time_mp"
+        echo "$pair $exec_time_mp"
 
         # Atualiza o arquivo com o tempo de execução paralelo
         # Procurando a linha correspondente ao par de entrada para adicionar o tempo MP
